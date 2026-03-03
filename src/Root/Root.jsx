e885b0c71fb287843b566397c403c9b213c9d8cf
shakilcstdev <shakilahamed.s2000@gmail.com>
2026-03-03 10:32:30 +0600

    Fix Navbar import path

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-// import Navbar from "../Components/Header/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../Components/Footer/Footer";
-import Navbar from "../components/Header/Navber";
+import Navbar from "../Components/Header/Navbar";  // ঠিক করা হয়েছে
 
 const Root = () => {
   return (
     <div className="mulish">
-      <div className=" md:max-w-7xl w-[94%] mx-auto">
+      <div className="md:max-w-7xl w-[94%] mx-auto">
         <Navbar></Navbar>
         <Outlet></Outlet>
       </div>
